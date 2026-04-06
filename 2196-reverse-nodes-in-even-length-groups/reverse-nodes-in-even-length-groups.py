c74e7334aef7c0c37522703a4255ad10163ddfaa
BYIRINGIRO Gad <gadb5743@gmail.com>
2026-04-06 15:52:34 +0200

    Time: 154 ms (64.58%) | Memory: 54.7 MB (96.53%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
@@ -14,35 +14,30 @@ class Solution:
                 prev = curr
                 curr = nxt
                 k -= 1
-            return prev, curr 
-        
+            return prev, curr
         dummy = ListNode(0)
         dummy.next = head
-        
-        prev_group_end = dummy
+        prev_end = dummy
+
         curr = head
-        group_size = 1
-        
+        size = 1
         while curr:
-            count = 0
             temp = curr
-            while temp and count < group_size:
+            cnt = 0
+            while temp and cnt < size:
                 temp = temp.next
-                count += 1
-            
-            if count % 2 == 0:
-                new_head, next_group_start = reverse(curr, count)
-                
-                prev_group_end.next = new_head
-                curr.next = next_group_start
-                
-                prev_group_end = curr
-                curr = next_group_start
+                cnt += 1
+
+            if cnt % 2 == 0:
+                new_head, next_start = reverse(curr, cnt)
+
+                prev_end.next = new_head
+                curr.next = next_start
+                prev_end = curr
+                curr = next_start
             else:
-                for _ in range(count):
-                    prev_group_end = curr
+                for _ in range(cnt):
+                    prev_end = curr
                     curr = curr.next
-            
-            group_size += 1
-        
+            size += 1
         return dummy.next
